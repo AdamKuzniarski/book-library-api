c0bedf4dfde499e56475d51991b823c0ed9da8c0
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-11-04 08:57:52 +0100

    add book.service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Book } from './entities/book.entity';
+import { CreateBookDto } from './dto/create-book.dto';
+import { randomUUID } from 'crypto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BooksService {
@@ -15,5 +18,25 @@ export class BooksService {
       throw new NotFoundException('Book not found');
     }
     return book;
+  }
+
+  create(dto: CreateBookDto): Book {
+    const book: Book = { id: randomUUID(), ...dto };
+    this.books.push(book);
+    return book;
+  }
+
+  update(id: string, dto: UpdateBookDto): Book {
+    const index = this.books.findIndex((book) => book.id === id);
+    if (index === -1) throw new NotFoundException('Book not found');
+    const updatedBook: Book = { id, ...dto };
+    this.books[index] = updatedBook;
+    return updatedBook;
+  }
+
+  remove(id: string): void {
+    const index = this.books.findIndex((book) => book.id === id);
+    if (index === -1) throw new NotFoundException('Book not found');
+    this.books.splice(index, 1);
   }
 }
