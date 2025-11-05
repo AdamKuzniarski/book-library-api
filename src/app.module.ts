039af18466750893143214e8030eaa873e1a77bf
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-11-05 10:34:14 +0100

    add CI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    BooksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'book_library',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,14 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
+@Entity({ name: 'books' })
 export class Book {
+  @PrimaryColumn('uuid')
   @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000' })
   id: string;
 
+  @Column({ length: 255 })
   @ApiProperty({ example: 'W pustyni i w puszczy/Durch Wüste und Wildnis' })
   title: string;
+
+  @Column({ length: 150 })
   @ApiProperty({ example: 'Henryk Sienkiewicz' })
   author: string;
 
+  @Column('int')
   @ApiProperty({ example: 1911 })
   publishedYear: number;
 }
