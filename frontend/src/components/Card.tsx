882f6a550af7d2b6e8128cae7c19173bd4f2f6bf
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-11-12 21:37:28 +0100

    add some styling

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -5,8 +5,8 @@ type CardProps = {
 };
 export default function Card({ title, author, publishedYear }: CardProps) {
   return (
-    <div className="flex flex-col border p-4 rounded-lg shadow-md">
-      <h3 className="text-xl font-semibold">{title}</h3>
+    <div className="flex flex-col border p-4 rounded-lg shadow-md text-2xl m-4">
+      <h3 className="text-3xl font-semibold">{title}</h3>
       <p className="text-gray-600">{author}</p>
       <p className="text-gray-600">{publishedYear}</p>
     </div>
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,31 @@
 import { Link, Route, Routes } from "react-router-dom";
 
+
 export default function NavBar() {
   return (
-    <div>
-      <nav>
-        <Link to="/" className="">Books</Link> |<Link to="/user" className="">User</Link>
-      </nav>
+    <>
+      <header className="sticky top-0 z-50 w-full border-b border-white/20 bg-gradient-to-t from-sky-500 to-indigo-500">
+        <nav className="mx-auto flex h-27 max-w-5xl items-center justify-center gap-3">
+          <Link
+            to="/"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Books
+          </Link>
+          <span className="text-white/60">|</span>
+          <Link
+            to="/user"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            User
+          </Link>
+        </nav>
+      </header>
 
       <Routes>
         <Route path="/" element={<h2>Books list</h2>} />
         <Route path="/user" element={<h2>User Page</h2>} />
       </Routes>
-    </div>
+    </>
   );
 }
